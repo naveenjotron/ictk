7e86ba48d17f332bf33d440c02f5e916a5ee0c1a
naveenjotron <45149952+naveenjotron@users.noreply.github.com>
2022-02-20 20:14:23 +0530

    Add files via upload

---
+++ MainProject/src/test/java/StepDefinition/LoginSteps.java
@@ -0,0 +1,84 @@
+package StepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import com.ictacademyofficial.scripts.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.FindBy;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+	WebDriver driver;
+	TestBase tb;
+	@Given("user is navigated to loginpage")
+	public void user_is_navigated_to_loginpage() {
+//		System.setProperty("webdriver.gecko.driver", "D:\\SoftwareTestingICTAK\\Selenium\\CommonFiles\\geckodriver.exe");
+//		FirefoxOptions options = new FirefoxOptions();  
+//		options.setHeadless(false);
+//		driver= new FirefoxDriver(options);
+//		driver.get("http://64.227.132.109/");
+//		driver.manage().window().maximize();
+		
+		TestBase tb;
+		tb = new TestBase();
+		driver = tb.onSetup();
+		
+		
+		System.out.println("Inside Step - user is on login page");
+		
+	
+	}
+
+	@When("user clicks on login button")
+	public void user_clicks_on_login_button() {
+		
+		driver.findElement(By.cssSelector("a.btn")).click();
+		System.out.println("clicked successfully");
+		
+	}
+		
+	
+
+	@Then("^user enters (.*) and (.*)$")
+	public void user_enters_username_and_password(String username,String password) {
+		
+		WebElement usrname= driver.findElement(By.cssSelector("div.input-group:nth-child(1) > input:nth-child(2)"));
+		usrname.sendKeys(username);
+		driver.findElement(By.cssSelector("div.input-group:nth-child(2) > input:nth-child(2)")).sendKeys(password);
+		System.out.println("USERNAME: " +username + " And PASSWORD: " + password);
+	
+		
+		System.out.println("Inside Step - enter username and password");
+	}
+
+	@And("clicks on signin button")
+	public void clicks_on_signin_button() {
+		
+		driver.findElement(By.cssSelector("button.bg-gradient-info:nth-child(1)")).click();
+
+		System.out.println("Inside Steps - clicks on signin button");
+	}
+
+	@Then("user is redirected to the home page")
+	public void user_is_redirected_to_the_home_page() throws InterruptedException {
+		
+		System.out.println("Inside Step - user is navigated to the home page");
+		
+		if(driver.getPageSource().contains("Sauce Labs Backpack")) {
+			
+			System.out.println("SUCCESSFULLY reached home page");
+		
+		} else {
+			System.out.println("FAILED to get text");
+		}
+
+	}
+}
---
+++ MainProject/target/test-classes/Features/login.feature
@@ -0,0 +1,13 @@
+Feature: feature to test login funtionality
+
+  Scenario Outline: Check login is succesful with valid credentials
+    Given user is navigated to loginpage
+    When user clicks on login button
+    Then user enters <username> and <password>
+    And clicks on signin button
+    Then user is redirected to the home page
+
+    Examples: 
+      | username   | password |
+      | useradmin  |    12345 |
+      #| superadmin |    12345 |
