ed967128b75944383d56c1e2e4180896c0bc2bf8
Arunodayam <100779265+Arunodayam@users.noreply.github.com>
2022-03-04 20:01:21 +0530

    ArunaChange

--- MainProject/src/test/java/adminSection/Pages/SuperadminAcad.java
+++ MainProject/src/test/java/adminSection/Pages/SuperadminAcad.java
@@ -1,7 +1,12 @@
 package adminSection.Pages;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import projectBase.Scripts.AdminLoginBase;
 
@@ -15,9 +20,27 @@ WebDriver driver;
 		AdminLoginBase ul;
 		ul= new AdminLoginBase(driver);
 		driver= ul.login();
-		
-		
+				
 }
-		
-		
+@Test(priority=1)
+public void courses() throws InterruptedException {
+Thread.sleep(5000);
+driver.getPageSource().contains("Dashboard");
+//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+driver.findElement(By.cssSelector("<div _ngcontent-wxd-c30=\"\" class=\"text-white text-center me-2 d-flex align-items-center justify-content-center\"><i _ngcontent-wxd-c30=\"\" class=\"material-icons opacity-10\">assignment_ind</i></div><span _ngcontent-wxd-c30=\"\" class=\"nav-link-text ms-1\">Academic Membership</span></a>)")).click();
+
+}
+@Test(priority=2)
+
+
+
+
+
+
+@AfterTest
+public void quitBrowser() throws IOException, InterruptedException {
+	Thread.sleep(10000);
+    driver.quit();
+}
 }
